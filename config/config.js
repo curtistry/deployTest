623f617281c3d1e48c1f53b6e9be03e8fc81bcbf
Curtis Carpio <curtiscarpio@gmail.com>
2023-11-16 13:17:11 -0500

    first commit

---
+++ config/config.js
@@ -0,0 +1,23 @@
+/*const config = {
+env: process.env.NODE_ENV || 'development', 
+port: process.env.PORT || 3000,
+jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+mongoUri: process.env.MONGODB_URI ||
+process.env.MONGO_HOST ||
+'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+(process.env.MONGO_PORT || '27017') +
+'/mernproject' 
+}
+export default config*/
+
+const config = {
+ env: process.env.NODE_ENV || 'development', 
+ port: process.env.PORT || 3000,
+ jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+ mongoUri: process.env.MONGODB_URI || "mongodb+srv://curtistry:thisissarta@curtiscomp229.le9uyhw.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"||
+process.env.MONGO_HOST ||
+ 'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+(process.env.MONGO_PORT || '27017') +
+ '/mernproject' 
+ }
+ export default config
